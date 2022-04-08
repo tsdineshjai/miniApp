96f2e0baaec00077ebeed1cf858446a7b08ed591
tanishka <tsdineshjai@gmail.com>
2022-04-08 17:16:40 +0530

    Updates

---
+++ precache-manifest.fa9563ea313a50e5a918c7c953627e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8e5089e38bdf9311ce",
+    "url": "/miniApp/static/css/main.127ad672.chunk.css"
+  },
+  {
+    "revision": "ff8e5089e38bdf9311ce",
+    "url": "/miniApp/static/js/main.ff8e5089.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/miniApp/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "48c42f8a7ddbe3dba839",
+    "url": "/miniApp/static/js/2.48c42f8a.chunk.js"
+  },
+  {
+    "revision": "6202b66f9596e6696d9f",
+    "url": "/miniApp/static/js/runtime~main.6202b66f.js"
+  },
+  {
+    "revision": "89ce64cfae5133f7b5d6d1b317423093",
+    "url": "/miniApp/static/media/lady.89ce64cf.jpg"
+  },
+  {
+    "revision": "cc5d5b86c17cd9de65f62eb7bb7f428c",
+    "url": "/miniApp/index.html"
+  }
+];
